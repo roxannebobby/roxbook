2f0553c272e98cc52560c2ac1927acf005fe55e2
Roxanne Weber <marie.sankofa@gmail.com>
2021-11-04 12:17:38 -0500

    basic setup complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get('https://randomuser.me/api/?results=5')
+			.then((resp) => {
+				setUsers(resp.data.results);
+			})
+			.catch((err) => console.error(err));
+	}, []);
+	return (
+		<div className='App'>
+			<h1>RoxBook, like Facebook with less drama!</h1>
+		</div>
+	);
 }
 
 export default App;
